05c5290a4c8f05e1c8343fd9c4ee4f270d02e821
VietNguyenDev <nguyenviet25102001@gmail.com>
2023-03-08 10:51:25 +0700

    fix user

--- app/http/controllers/user/deleteUser.js
+++ app/http/controllers/user/deleteUser.js
@@ -2,7 +2,7 @@ const userService = require('../../services/user');
 
 async function deleteUserController(req, res) {
     const {id} = req.params;
-    await deleteUser({id});
+    await userService.deleteUser({id});
 
     return res.status(200).send({ message: "User  deleted successfully" })
 }
--- app/routes/user.js
+++ app/routes/user.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const { user: userController } = require('../http/controllers');
 const upload = require('../http/middlewares/uploadFile');
+const { auth } = require('../http/middlewares');
+const checkPermission = require('../http/middlewares/permission');
 
 const router = express.Router();
 
 router.get('/users', userController.getUsers);
 router.put('/users', upload.single('userImg'), userController.updateUser);
-
+router.post('/users/:id', auth, checkPermission, userController.deleteUser )
 
 module.exports = router;
