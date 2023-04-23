9a83e7efc8a3b5843d6d2b9f00b78b3167ccc1ed
HanKienjd <kienp544@gmail.com>
2023-04-24 06:44:34 +0700

    fix api shipping

---
+++ app/routes/shipping.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const { shipping: shippingController } = require('../http/controllers');
+const auth = require('../http/middlewares/auth');
+const router = express.Router();
+
+const checkPermission = require('../http/middlewares/permission');
+
+router.get('/shipping/:userId', auth, shippingController.getShippingByUser);
+router.delete('/shipping/:id', auth, shippingController.delete);
+
+router.put('/shipping/:id', auth, shippingController.update);
+router.post('/shipping/:userId', auth, shippingController.create);
+
+module.exports = router;
--- database/migrations/11_create_shipping_detail_table.js
+++ database/migrations/11_create_shipping_detail_table.js
@@ -9,16 +9,16 @@ exports.up = async (knex) => {
       .unsigned()
       .references('shipping.id')
       .notNullable();
-    table.string('firstName', 50).collate('utf8_general_ci').notNullable();
-    table.string('lastName', 50).collate('utf8_general_ci').notNullable();
-    table.string('address1', 255).collate('utf8_general_ci').notNullable();
-    table.string('address2', 255).collate('utf8_general_ci');
-    table.string('city', 50).collate('utf8_general_ci').notNullable();
-    table.string('state', 50).collate('utf8_general_ci').notNullable();
-    table.string('country', 50).collate('utf8_general_ci').notNullable();
+    table.integer('userId').unsigned().references('users.id').notNullable();
+    table.string('fullName', 50).collate('utf8_general_ci').notNullable();
+    table.string('address', 255).collate('utf8_general_ci');
+    table.string('province', 255).collate('utf8_general_ci').notNullable();
+    table.string('district', 255).collate('utf8_general_ci').notNullable();
+    table.string('ward', 255).collate('utf8_general_ci').notNullable();
     table.string('postcode', 50).collate('utf8_general_ci').notNullable();
-    table.string('shippingTotal', 50).collate('utf8_general_ci').notNullable();
-    table.string('shippingTax', 50).collate('utf8_general_ci').notNullable();
+    table.string('phone', 255).collate('utf8_general_ci').notNullable();
+    table.integer('shippingTotal').notNullable();
+    table.integer('shippingTax', 10).notNullable();
     table.timestamp('created_at').defaultTo(knex.raw('CURRENT_TIMESTAMP'));
     table
       .timestamp('updated_at')
